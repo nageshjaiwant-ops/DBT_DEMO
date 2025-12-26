1380da4211d93b274a05ec4ae047350ce4a5d655
nagesh.jaiwant@gmail.com <nagesh.jaiwant@gmail.com>
2025-12-26 14:46:58 +0000

    trip_fact_model_created

---
+++ models/trip_fact.sql
@@ -0,0 +1,20 @@
+WITH trips as (
+
+select 
+ride_id,
+rideable_type,
+DATE(TO_TIMESTAMP(STARTED_AT)) AS TRIP_DATE,
+start_station_id,
+END_STATION_ID,
+MEMBER_CASUAL,
+TIMESTAMPDIFF(SECOND,TO_TIMESTAMP(STARTED_AT),TO_TIMESTAMP(ENDED_AT)) AS TRIP_DURATION
+
+
+from {{ source('demo', 'bike') }}
+
+where ride_id != 'ride_id'
+
+limit 10
+)
+
+select * from trips
